13689acbef18cc81b13e39654e8e5738a07e58ff
Fatma Orsun <116498723+fators22@users.noreply.github.com>
2022-11-07 15:25:51 +0000

    "Add Class Files"

---
+++ Car/Car.java
@@ -0,0 +1,7 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+//design your Car class
+//write all code for your Car class here
+//test your Car class with the CarRunner
---
+++ Cookie/CookieJar.java
@@ -0,0 +1,7 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+//design your CookieJar class
+//write all code for your CookieJar class here
+//test your CookieJar class with the CookieJarRunner
